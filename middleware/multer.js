7e2579679ca6940c4afbdeffce7fc139d6066a69
Arshad Nasir <qureshiarshad947@gmail.com>
2025-08-09 18:49:54 +0500

    rttyr

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,8 +1,15 @@
 const multer = require("multer");
+const path = require("path");
+
+// /tmp folder (Vercel serverless temporary storage)
 const storage = multer.diskStorage({
-  filename: function (req,file,cb) {
-    cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, "/tmp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
+
 const upload = multer({ storage: storage });
 module.exports = upload;
